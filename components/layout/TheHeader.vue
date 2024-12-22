772dc88054d02e76d50547031b4bd43044872027
AbdelfattahSekak <abdelfattah.sekak@consulting-for.accor.com>
2024-12-22 13:11:12 +0100

    refactor: update TripItinerary and TheHeader components for improved layout and styling

--- components/layout/TheHeader.vue
+++ components/layout/TheHeader.vue
@@ -3,7 +3,7 @@
     class="h-[20vh] p-6 flex flex-col items-center justify-center bg-slate-50 border-b-1 w-full"
     role="banner"
   >
-    <h1 class="text-[32px] font-bold my-4 sm:mb-4 text-black tracking-tight">
+    <h1 class="text-[32px] font-bold mb-4 sm:mb-4 text-black tracking-tight">
       Your Next Adventure Awaits
     </h1>
     <slot></slot>
