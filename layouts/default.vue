3da30030687a82b9809b6906789855fb17dc5b34
AbdelfattahSekak <abdelfattah.sekak@consulting-for.accor.com>
2024-12-23 20:18:11 +0100

    feat: add Analytics and SpeedInsights components to default layout; update package.json with new dependencies

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,4 +4,10 @@
     <slot />
     <TheFooter />
   </div>
+  <Analytics />
+  <SpeedInsights />
 </template>
+<script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+</script>
