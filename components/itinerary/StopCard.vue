4431479a1ce0cef707ccfae241604a77d70488ea
AbdelfattahSekak <abdelfattah.sekak@consulting-for.accor.com>
2024-12-27 10:13:06 +0100

    feat: enhance StopCard component; improve layout and handle missing photos gracefully

--- components/itinerary/StopCard.vue
+++ components/itinerary/StopCard.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="flex flex-col gap-2">
-    <div></div>
     <h2 class="text-xl font-bold">{{ stop.title }}</h2>
-    <p class="text-sm text-text-secondary flex items-center gap-2 mt-1">
+    <p class="text-sm text-text-secondary flex items-center gap-2">
       <i class="pi pi-map-marker text-primary"></i>
       {{ stop.address }}
     </p>
   </div>
-  <Card class="shadow-xl bg-gray-50">
+  <Card class="shadow-xl mt-2 bg-gray-50">
     <template #header>
-      <StopPhoto :photos="stop.locationInfo.photos" />
+      <StopPhoto :photos="stop.locationInfo?.photos || []" />
     </template>
     <template #content>
       <div class="p-2 flex flex-col gap-4">
@@ -23,6 +22,7 @@
       </div>
     </template>
   </Card>
+  <Divider />
 
   <Dialog
     v-model:visible="showModal"
