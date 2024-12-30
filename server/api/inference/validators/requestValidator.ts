1c89173872cde0ab31c9be90f6377689abe9aa9d
AbdelfattahSekak <abdelfattah.sekak@consulting-for.accor.com>
2024-12-30 12:20:25 +0100

    feat: enhance request validation; improve error messages and add length checks for start and destination

--- server/api/inference/validators/requestValidator.ts
+++ server/api/inference/validators/requestValidator.ts
@@ -1,12 +1,16 @@
-import type { SearchFormData } from '~/types';
+import type { SearchFormData } from "~/types";
 
 export function validateRequestBody(body: SearchFormData) {
   if (!body.start || !body.destination) {
-    return new Error('Start and destination locations are required');
+    return new Error("Start and destination locations are required");
+  }
+
+  if (body.start.length > 50 || body.destination.length > 50) {
+    return new Error("Start or destination are not correct");
   }
 
   if (!body.nbStops || body.nbStops < 1 || body.nbStops > 10) {
-    return new Error('Number of stops must be between 1 and 10');
+    return new Error("Number of stops must be between 1 and 10");
   }
 
   return null;
